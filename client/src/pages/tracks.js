f5f1bceedcad94a9f6f47c72f7b1b88d1e8ebb73
Jiro Ghianni <jiro.ghianni@iodigital.com>
2022-09-28 15:42:56 +0200

    set up query in client

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react"
+import { Layout } from "../components"
+// always import this first
+import { gql } from "@apollo/client"
+
+const TRACKS = gql`
+  # Query goes here
+  query GetTracksSandbox {
+    tracksForHome {
+      id
+      length
+      title
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+    }
+  }
+`
+
+console.log(TRACKS)
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
-};
+  return <Layout grid> </Layout>
+}
 
-export default Tracks;
+export default Tracks
