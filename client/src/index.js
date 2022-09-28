20c74dd7384bbaaf9eee52367ac2b70bb5aa50f0
Jiro Ghianni <jiro.ghianni@iodigital.com>
2022-09-28 15:31:21 +0200

    set up Apollo client + provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
+import React from "react"
+import ReactDOM from "react-dom"
+import GlobalStyles from "./styles"
+import Pages from "./pages"
+// We first need to install two packages: graphql and @apollo/client
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
+
+const client = new ApolloClient({
+  // the location of our GraphQL server.
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </ApolloProvider>,
+  document.getElementById("root")
+)
